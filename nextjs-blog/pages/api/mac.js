1d935dc89ac9ac5c18c4de26b5388051e9e67372
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2022-05-09 13:34:23 +0530

    new python code

--- nextjs-blog/pages/api/mac.js
+++ nextjs-blog/pages/api/mac.js
@@ -10,12 +10,18 @@ export default function handler(req, res) {
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
         origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-     });
+    });
+
+    // const body = {
+    //     "projects" : ["project 1", "project 2", "project 3", "project 4", "project 5", "project 6", "project 7"],
+    //     "ers" : [120, 100, 40, 50, 60, 80, 200],
+    //     "macs" : [40, 100, -10, 10, 15, 25, 30]
+    // }
 
     const body = {
-        "projects" : ["project 1", "project 2", "project 3", "project 4", "project 5", "project 6", "project 7"],
-        "ers" : [120, 100, 40, 50, 60, 80, 200],
-        "macs" : [40, -20, -10, 10, 15, 25, 30]
+        projects: [ '5 diesel', 'Passenger', 'TJHbj','ewdwe','vfgvf'],
+        ers: [ 751, 10751, 233,443,43],
+        macs: [ 10, 100,434,54,43 ]
     }
 
     const options = {
@@ -32,10 +38,40 @@ export default function handler(req, res) {
             const _body = body;
 
             if (_body.error) {
-                res.send(_body.error);
+                // res.send(_body.error);
             } else if (_body) {
-                return res.send({data: _body});
+                // return res.send({data: _body});
             }
         }
     });
+
+    request(options, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        } else {
+            const _body = body;
+
+            if (_body.error) {
+                // res.send(_body.error);
+            } else if (_body) {
+                // return res.send({data: _body});
+            }
+        }
+    });
+
+    request(options, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        } else {
+            const _body = body;
+
+            if (_body.error) {
+                // res.send(_body.error);
+            } else if (_body) {
+                // return res.send({data: _body});
+            }
+        }
+    });
+
+
 }
